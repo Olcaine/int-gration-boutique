b48d8263344a53399048767362c9ec2d9f13dd9f
hermelen <hermelen.peris@gmail.com>
2018-07-17 14:26:04 +0200

    before pull

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
   $('main h3:contains("BUY")').addClass("kk");
   $('main h3:contains("ON SALE")').addClass("pp");
   $('main h3:contains("SOLD OUT")').addClass("gy");
-  $('footer h3').addClass("titleFooter");
+  // $('footer h3').addClass("titleFooter");
 })
