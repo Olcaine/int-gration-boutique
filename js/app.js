6f455bf500dbd174d3c769ab57301896730a818c
hermelen <hermelen.peris@gmail.com>
2018-07-18 09:40:25 +0200

    articles added by catalog_x10.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+  console.log(catalog);
+
+  for (var i = 0; i < catalog.length; i++) {
+    $('#catalog').append(`
+      <div class="col-6 col-sm-4 col-md-3 col-lg-2">
+        <article>
+          <img class="img-fluid" src="${catalog[i].picture}" alt="">
+          <div class="article-div">
+            <h3>${catalog[i].statut}</h3>
+            <h4>${catalog[i].name}</h4>
+            <h5>${catalog[i].price}$</h5>
+          </div>
+        </article>
+      </div>
+    `)
+  }
+
   $('main h3:contains("BUY")').addClass("kk");
   $('main h3:contains("ON SALE")').addClass("pp");
   $('main h3:contains("SOLD OUT")').addClass("gy");
---
+++ js/catalog_x10.js
@@ -0,0 +1,91 @@
+var catalog = [
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"BUY",
+  name :"Winter Scarf",
+  price :180.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"BUY",
+  name :"Winter Scarf",
+  price :180.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Summer Sandals",
+  price :220.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"BUY",
+  name :"Red Had and Worf",
+  price :20.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"BUY",
+  name :"Green Coat",
+  price :220.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Dark Coat",
+  price :220.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"BUY",
+  name :"Blue Troursers",
+  price :220.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"SOLD OUT",
+  name :"Blue Sweater",
+  price :220.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"BUY",
+  name :"Blue Shirt",
+  price :220.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Blue backpack",
+  price :120.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Beach Chair",
+  price :80.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Decoration Letter",
+  rice :120.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Sutton Bench",
+  price :120.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"BUY",
+  name :"Red Troursers",
+  price :220.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Winter Sandals",
+  price :220.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Beach Chair",
+  price :80.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"BUY",
+  name :"Winter Scarf",
+  price :180.00},
+
+  {picture :"https://picsum.photos/300/400/?random",
+  statut :"ON SALE",
+  name :"Winter Scarf",
+  price :120.00}
+]
